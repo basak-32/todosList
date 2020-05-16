a729a4b7cb68ab7d291ce070bf5b766bc123d244
basak-32 <ranajit1998basak@gmail.com>
2020-05-17 04:17:26 +0530

    Added search box

--- script.js
+++ script.js
@@ -1,14 +1,58 @@
 list = document.querySelector('ul');
 button = document.querySelector('button');
 inputField = document.querySelector('.add-input-field');
-resetButton = document.querySelector('.reset-button');
+resetButton = document.querySelector('.reset-button')
+searchBox = document.querySelector('.search-box');
+
+
+// console.log(searchBox);
+// console.log(searchBox.value);
+
+const search = () => {
+    Array.from(list.children).forEach(todo => {
+        // console.log(todo.textContent);
+
+        if (!todo.textContent.toLowerCase().includes(searchBox.value.toLowerCase())) {
+            todo.classList.add('d-none');
+        }
+
+        if (todo.textContent.toLowerCase().includes(searchBox.value.toLowerCase())) {
+            todo.classList.remove('d-none');
+        }
+    })
+}
+
+searchBox.addEventListener('keyup', search);
+
+
+
+// searchBox.addEventListener('keyup', () => {
+//     // console.log(searchBox.value);
+//     // console.log(list.children);
+
+//     Array.from(list.children).forEach(todo => {
+//         // console.log(todo.textContent);
+
+//         if (!todo.textContent.toLowerCase().includes(searchBox.value.toLowerCase())) {
+//             todo.classList.add('d-none');
+//         }
+
+//         if (todo.textContent.toLowerCase().includes(searchBox.value.toLowerCase())) {
+//             todo.classList.remove('d-none');
+//         }
+//     })
+// })
+
+// Array.from(list.children).forEach(todo => {
+//     console.log(todo.textContent);
+// });
+
 
 
 // console.log(button);
 // console.log(inputField);
 
-button.addEventListener('click', event => {
-    // console.log(event);
+const addTodoAfterClick = () => {
     if (inputField.value.trim()) {
         list.innerHTML += `
             <li class="list-group-item">
@@ -18,13 +62,9 @@ button.addEventListener('click', event => {
             `
     }
     inputField.value = '';
-    
-    // console.log(list);
-})
+}
 
-inputField.addEventListener('keypress', event => {
-    // console.log(event);
-
+const addTodoAfterEnter = () => {
     if (event.keyCode === 13) {
         if (inputField.value.trim()) {
             list.innerHTML += `
@@ -36,9 +76,45 @@ inputField.addEventListener('keypress', event => {
         }
         inputField.value = '';
     }
-})
+}
+
+// button.addEventListener('click', event => {
+//     // console.log(event);
+//     if (inputField.value.trim()) {
+//         list.innerHTML += `
+//             <li class="list-group-item">
+//                 ${inputField.value.trim()} 
+//                 <i class="fas fa-trash text-primary delete" style="float: right;" ></i>
+//             </li>
+//             `
+//     }
+//     inputField.value = '';
+    
+//     // console.log(list);
+// })
+
+
+// inputField.addEventListener('keypress', event => {
+//     // console.log(event);
+
+//     if (event.keyCode === 13) {
+//         if (inputField.value.trim()) {
+//             list.innerHTML += `
+//                 <li class="list-group-item">
+//                     ${inputField.value.trim()} 
+//                     <i class="fas fa-trash text-primary delete" style="float: right;" ></i>
+//                 </li>
+//                 `
+//         }
+//         inputField.value = '';
+//     }
+// })
 
 // console.log(list);
+
+button.addEventListener('click', addTodoAfterClick);
+inputField.addEventListener('keypress', addTodoAfterEnter);
+
 
 list.addEventListener('click', event => {
     // console.log(event);
@@ -49,12 +125,10 @@ list.addEventListener('click', event => {
     if (event.target.classList.contains('delete')) {
         event.target.parentElement.remove();
     }
-   
-    
 });
 
 // console.log(list.children);
 
 resetButton.addEventListener('click', () => {
     list.innerHTML = '';
-})
+});
